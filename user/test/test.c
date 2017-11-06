837f6e8c2e54889ab0e4165d414361e43a6bc92f
yogurtkun <yogurtkun@126.com>
2017-11-06 11:09:27 -0500

    add test program

---
+++ user/test/test.c
@@ -0,0 +1,31 @@
+#include <linux/unistd.h>
+#include <sys/syscall.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include "stdio.h"
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "test.h"
+
+#define __NR_sched_setscheduler 119
+struct sched_param {
+	int sched_priority;
+};
+
+int main(int argc, char const *argv[])
+{
+
+	pid_t p = getpid();
+	int policy = 6;
+	struct sched_param param;
+	param.sched_priority = 101;
+
+
+	syscall(__NR_sched_setscheduler,p,policy,&param);
+
+	while(1);
+	
+	return 0;
+}
