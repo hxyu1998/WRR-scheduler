af7b47f8161a88941678e9a91641e2fe9db1f4ef
yogurtkun <yogurtkun@126.com>
2017-11-07 15:26:01 -0500

    finish daemon program

--- user/test/daemon.c
+++ user/test/daemon.c
@@ -7,9 +7,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/ioctl.h>
+
+#include <fcntl.h>
+
+#include <sys/stat.h>
 
 #define __NR_get_wrr_info 244
 #define __NR_SET_WRR_WEIGHT 245
+
+#define MAX_CPUS 8
 
 
 struct wrr_info {
@@ -57,10 +64,13 @@ void daemon_mode(void)
 
 int main(int argc, char const *argv[])
 {
+	fprintf(stdout, "Start to work\n" );
 	daemon_mode();
 
 	struct wrr_info data;
 	int i = 0;
+
+	fprintf(stdout, "Daemon process start to work\n" );
 
 	while(1){
 		syscall(__NR_get_wrr_info,&data);
@@ -68,7 +78,8 @@ int main(int argc, char const *argv[])
 		for(i = 0 ; i < data.num_cpus; i ++){
 			fprintf(stdout, "%d cpu has %d tasks and total weight is %d\n",i,data.nr_running[i],data.total_weight[i] );
 		}
-		sleep(100000);
+		fprintf(stdout, "**********************************************\n");
+		usleep(1000);
 	}
 
 	return 0;
