8fb650380688a26a499992a743e9d12400fcbc1d
kaelchan <kaelchan@hotmail.com>
2017-11-11 21:13:20 -0500

    modify load and boot sh

--- load_and_boot.sh
+++ load_and_boot.sh
@@ -1,9 +1,7 @@
-if [ -d "build-debug" ]; then
-# if [ -d "build-master" ]; then
+if [ -d "build-master" ]; then
 	# Control will enter here if $DIRECTORY doesn't exist.
 	tools/make_bootimg
 	adb reboot bootloader
 	fastboot devices
-	fastboot boot build-debug/boot.img
-	# fastboot boot build-master/boot.img
+	fastboot boot build-master/boot.img
 fi
